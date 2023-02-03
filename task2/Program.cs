a00b2d540e9f1745ced130ecc1ad8723f0f792f7
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-02-03 21:39:24 +0300

    task2 completed

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+System.Console.WriteLine("Введите любое число");
+string number = Console.ReadLine();
+int number1 = Convert.ToInt32(number);
+if(number1 <= 99)
+{
+    System.Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+     System.Console.WriteLine($"Третья цифра заданного числа {number[2]}");
+}
+
+
